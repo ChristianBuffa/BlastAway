3d75abedc0266d68d990637e06af36ee6acbe9c3
Luca Gunnella <luca.gunnella@gmail.com>
2023-07-06 13:23:11 +0200

    fix interaction check

--- BlastAway/Assets/Scripts/Interaction/PlayerInteract.cs
+++ BlastAway/Assets/Scripts/Interaction/PlayerInteract.cs
@@ -10,14 +10,17 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] private KeyCode interactKey = KeyCode.E;
     [SerializeField] private KeyCode fireKey = KeyCode.Mouse0;
 
-    [SerializeField] private int maxCapacity;
+    [SerializeField] private float maxInteractDistance = 5f;
+    [SerializeField] private int maxCapacity = 10;
 
+    private Camera playerCamera;
     public Transform actionPoint;
     private List<Item> inventory = new List<Item>();
     private Item equippedItem;
 
     private void Awake()
-    {
+    { 
+        playerCamera = GetComponentInChildren<Camera>();
         if (inventory.Any())
             equippedItem = inventory[0];
     }
@@ -25,15 +28,16 @@ public class PlayerInteract : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(interactKey))
-            CheckItemsForInteraction();
+            InteractionCheck();
 
         if (Input.GetKeyDown(fireKey))
             UseItem();
     }
 
-    private void CheckItemsForInteraction()
+    private void InteractionCheck()
     {
-        Physics.BoxCast(transform.position, new Vector3(1, 1, 1), Vector3.forward, out RaycastHit hit);
+        var ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        Physics.Raycast(ray, out RaycastHit hit, maxInteractDistance);
         var interactable = hit.collider.GetComponent<IInteractable>();
         interactable?.OnInteract(this);
     }
