fc802c3ec8e4c7bb53f1fb644ce27c72592a586f
ChristianBuffa <christian.b2402@gmail.com>
2023-07-07 13:59:47 +0200

    fixed swing animation

--- BlastAway/Assets/Scripts/Items/Axe.cs
+++ BlastAway/Assets/Scripts/Items/Axe.cs
@@ -5,10 +5,19 @@ public class Axe : Item
 {
     public int damage;
 
+    private Animator anim;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim = GetComponent<Animator>();
+            anim.SetTrigger("SwingTrigger");
+        }
+    }
+
     public override void Use(PlayerInteract player)
     {
-        
-        player.EquippedItem.GetComponentInChildren<Animator>().SetTrigger("SwingTrigger");
         var hitColliders = Physics.OverlapSphere(player.actionPoint.position, 1f, LayerMask.GetMask("Destructible"));
 
         foreach (Collider collider in hitColliders)
