0be085b5b5285c4febd3276b53af4f7705c84f34
ChristianBuffa <christian.b2402@gmail.com>
2023-07-08 12:45:30 +0200

    added flame effect and flame thrower damage

--- BlastAway/Assets/Scripts/Interaction/Destructible.cs
+++ BlastAway/Assets/Scripts/Interaction/Destructible.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
     public int maxHp;
+    public bool isFlammable;
 
+    private bool isBurning = false;
+    private int burningTimer = 0;
     protected int currentHp;
 
     public virtual void OnTakeDamage(int damage)
@@ -18,6 +22,38 @@ public class Destructible : MonoBehaviour
         }
     }
 
+    public virtual void OnDamageOverTime(int damage, int dps, int time, int waitTime)
+    {
+        if (isFlammable)
+        {
+            currentHp -= damage;
+            burningTimer++;
+            Debug.Log(currentHp);
+
+            if (burningTimer > 3)
+            {
+                Debug.Log("burning");
+                StartCoroutine(DPS(dps, time, waitTime));
+            }
+        }
+    }
+
+    IEnumerator DPS(int dps, int time, int waitTime)
+    {
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < time; i++)
+        {
+            currentHp -= dps;
+            yield return new WaitForSeconds(waitTime);
+
+            if (currentHp <= 0)
+            {
+                OnDeath();
+            }
+        }
+    }
+    
     protected virtual void OnDeath()
     {
         Debug.Log("dead");
--- BlastAway/Assets/Scripts/Items/FlameThrower.cs
+++ BlastAway/Assets/Scripts/Items/FlameThrower.cs
@@ -1,11 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FlameThrower : Item
 {
+    public int damage;
+    public int dps;
+    public int waitTime;
+    public int burningTime;
+
+    public GameObject fireParticle;
+
+    private float mouseDownTimer = 0;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            fireParticle.SetActive(true);
+            mouseDownTimer++;
+        }
+        else
+        {
+            fireParticle.SetActive(false);
+        }
+
+        if (mouseDownTimer > 150)
+        {
+            mouseDownTimer = 0;
+            Debug.Log("over 30");
+            Use(GetComponentInParent<PlayerInteract>());
+        }
+    }
+
     public override void Use(PlayerInteract player)
     {
-        
+        var hitColliders = Physics.OverlapSphere(player.actionPoint.position, 1f, LayerMask.GetMask("Destructible"));
+
+        foreach (Collider collider in hitColliders)
+        {
+            Debug.Log("Flame thrower hit " + collider.name);
+
+            if (collider.gameObject.GetComponent<Destructible>())
+            {
+                collider.gameObject.GetComponent<Destructible>().OnDamageOverTime(damage, dps, burningTime, waitTime);
+            }
+        }
     }
 }
